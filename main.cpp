3729e7cc6bbed9fecef5ec7c90d917edd8cff4fe
lennart.krebs <contact@lennartnicolas.de>
2025-09-29 23:57:19 +0200

    Some more layout

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <separation_engine.h>
+
+int main()
+{
+    using namespace stemsmith;
+
+    separation_engine engine("path/to/model", 4);
+    return 0;
+}
---
+++ src/separation_engine.cpp
@@ -0,0 +1,9 @@
+#include "separation_engine.h"
+
+namespace stemsmith {
+
+separation_engine::separation_engine(std::string_view model_path, size_t num_workers)
+{
+}
+
+} // namespace stemsmith
